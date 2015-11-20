85ce1bcd6910e434dc4b1f939ed7cdec7c25b9b1
Jitendra Kumar Gupta <jgupta@walmartlabs.com>
2015-11-20 23:32:01 +0530

    Delete test1.rb

--- test1.rb
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env ruby
-
-require_relative 'token'
-
-
-azure_dns = OAuth::Token.new('3cbcc3d3-094d-4006-9849-0d11d61f484d', '27b10960-2be3-4ed6-94d6-519747fd7073',
-                             '875458f6-8054-42c6-afd3-3c50acea2a23', '7\MB2g3KtZ=f8YD;Ci7J')
-azure_dns_token = azure_dns.get_token
-puts request_token
